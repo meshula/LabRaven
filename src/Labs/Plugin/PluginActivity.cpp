fb515a320f4d23aea88c342746895e4c1d429b54
Nick Porcino <meshula@hotmail.com>
2024-11-17 18:00:00 -0800

    TestPlugin is successfully dynamically loaded

--- src/Labs/Plugin/PluginActivity.cpp
+++ src/Labs/Plugin/PluginActivity.cpp
@@ -6,14 +6,22 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui.h"
 
+#define CR_NO_ROLLBACK
+#define CR_HOST
+#include "cr.h"
+
 #include <sys/stat.h>
+#include <filesystem>
 #include <vector>
 
 namespace lab {
 
 struct PluginData {
     std::string name;
-    bool enabled;
+    std::string plugin_filename;
+    bool enabled = false;
+    bool loaded = false;
+    cr_plugin ctx;
 };
 
 struct PluginActivity::data
@@ -32,19 +40,55 @@ struct PluginActivity::data
 PluginActivity::PluginActivity() {
     _self = new data;
 
-    _self->plugins.push_back({"Plugin 1", true});
-    _self->plugins.push_back({"Plugin 2", false});
-    _self->plugins.push_back({"Plugin 3", true});
-
-    // set _self->temp_found_plugins_dir to true if the plugins directory was found
-    // test for the existance of the plugins directory
+    // look in the plugins directory for any plugins, recognizable by a .dll or .so extension.
     std::string dir = _self->plugins_dir();
+
+    std::vector<std::filesystem::path> found_plugins;
     struct stat info;
     if (stat(dir.c_str(), &info) == 0 && S_ISDIR(info.st_mode)) {
+        // set _self->temp_found_plugins_dir to true if the plugins directory was found
         _self->found_plugins_dir = true;
-    } else {
+        // use std::filesystem to gather any plugins in the plugins directory
+        for (const auto& entry : std::filesystem::directory_iterator(dir)) {
+            if (entry.is_regular_file()) {
+                std::string ext = entry.path().extension().string();
+                if (ext == ".dll" || ext == ".so") {
+                    found_plugins.push_back(entry.path());
+                }
+            }
+        }
+    }
+    else {
         _self->found_plugins_dir = false;
     }
+
+    // load each plugin found in the plugins directory
+    for (const auto& plugin : found_plugins) {
+        PluginData pd;
+        pd.name = plugin.filename().string();
+        pd.plugin_filename = plugin.filename().string();
+
+        // load the plugin
+        pd.loaded = cr_plugin_open(pd.ctx, plugin.string().c_str());
+        pd.enabled = pd.loaded;
+
+        // the plugin may have a function const char* PluginName() that returns 
+        // the name of the plugin, fetch it using cr_so_symbol.
+        if (pd.loaded) {
+            cr_plugin_update(pd.ctx);   // causes the plugin to load.
+            auto plugInNameFn = cr_so_symbol<const char*(*)(void)>(pd.ctx, "PluginName");
+            if (plugInNameFn) {
+                pd.name = plugInNameFn();
+            }
+        }
+
+        _self->plugins.push_back(pd);
+    }
+
+    // add some dummy plugins for testing the UI
+    _self->plugins.push_back({"Plugin 1", "built in", true});
+    _self->plugins.push_back({"Plugin 2", "built in", false});
+    _self->plugins.push_back({"Plugin 3", "built in", true});
 
     activity.RunUI = [](void* instance, const LabViewInteraction* vi) {
         static_cast<PluginActivity*>(instance)->RunUI(*vi);
@@ -71,7 +115,17 @@ void PluginActivity::RunUI(const LabViewInteraction& interaction) {
 
     // Display the list of plugins, with a checkbox to enable/disable each one
     for (auto& plugin : _self->plugins) {
-        ImGui::Checkbox(plugin.name.c_str(), &plugin.enabled);
+        // if the plugin was not loaded, display it in red
+        if (!plugin.loaded) {
+            ImGui::TextColored(ImVec4(1, 0, 0, 1), "%s", plugin.name.c_str());
+            ImGui::SameLine();
+            ImGui::TextUnformatted(" (not loaded)");
+        }
+        else {
+            ImGui::Checkbox(plugin.name.c_str(), &plugin.enabled);
+            ImGui::SameLine();
+            ImGui::Text(" (%s)", plugin.plugin_filename.c_str());
+        }
     }
 
     ImGui::End();
--- src/Labs/Test/TestPlugin.cpp
+++ src/Labs/Test/TestPlugin.cpp
@@ -1,4 +1,6 @@
 
+#include "Labs/Plugin/cr.h"
+
 // set up PLUGIN_API with declspec or attribute per platform
 #if defined(_WIN32)
 #define PLUGIN_API __declspec(dllexport)
@@ -8,5 +10,18 @@
 
 // a single exported test function that returns the name of the plugin.
 extern "C" PLUGIN_API const char* PluginName() {
-    return "TestPluginx";
+    return "TestPlugin!";
+}
+
+// This interface is mandated by the cr.h library.
+extern "C" PLUGIN_API int cr_main(struct cr_plugin *ctx, enum cr_op operation) {
+    switch (operation) {
+        case CR_LOAD:   // loading back from a reload
+        case CR_UNLOAD: // preparing to a new reload
+        case CR_CLOSE:  // the plugin will close and not reload anymore
+        case CR_STEP:   // the plugin will do a step of work
+            return CR_NONE; // no error to report
+        default:
+            return CR_OTHER; // unknown operation
+    }
 }
